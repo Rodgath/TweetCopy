ed6bdfc7914a19e2819c02d8d0ad0b67f0fbfd87
rodgath <rodgath@gmail.com>
2021-06-15 22:01:36 +0300

    added 'getSiblings' function

--- app.js
+++ app.js
@@ -34,6 +34,18 @@ const copyToClipboard = (text) => {
     }
 }
 
+
+const getSiblings = (elem) => {
+    let siblings = [];
+    let sibling = elem.parentNode.firstChild;
+    for (; sibling; sibling = sibling.nextSibling) {
+        if (sibling.nodeType === 1 && sibling !== elem) {
+            siblings.push(sibling);
+        }
+    }
+    return siblings;
+};
+
 /* Multiple tweet statuses */
 const tweetArticles = () => {}
 
